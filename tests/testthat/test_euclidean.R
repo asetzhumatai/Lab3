495a9dd3d4f23435ae27f3a4579ebf78141dc0b5
Aset <asetzhumatai.00@gmail.com>
2025-09-22 19:55:08 +0200

    Add more test cases for euclidean function

--- tests/testthat/test_euclidean.R
+++ tests/testthat/test_euclidean.R
@@ -3,6 +3,7 @@ test_that("GDC is calculated correctly.", {
   expect_equal(euclidean(123612, 13892347912), 4)
   expect_equal(euclidean(100, 1000), 100)
   expect_equal(euclidean(-100, 1000), 100)
+  expect_equal(euclidean(0, 100), 100)
 })
 
 
@@ -10,5 +11,10 @@ test_that("Wrong input throws an error.", {
   expect_error(euclidean("100", 1000))
   expect_error(euclidean(100, "1000"))
   expect_error(euclidean(TRUE, "1000"))
+  expect_error(euclidean("", 100))
+  expect_error(euclidean(NA, 100))
+  expect_error(euclidean(10.5, 20))
+  expect_error(euclidean("ad", 10))
+  expect_error(euclidean(Inf, 100))
 })
 
