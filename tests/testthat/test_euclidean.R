364a384767dfd49e2bbb6656fd661542d535f74f
Aset <asetzhumatai.00@gmail.com>
2025-09-22 20:45:38 +0200

    update test cases euclidean

--- tests/testthat/test_euclidean.R
+++ tests/testthat/test_euclidean.R
@@ -13,7 +13,6 @@ test_that("Wrong input throws an error.", {
   expect_error(euclidean(TRUE, "1000"))
   expect_error(euclidean("", 100))
   expect_error(euclidean(NA, 100))
-  expect_error(euclidean(10.5, 20))
   expect_error(euclidean("ad", 10))
   expect_error(euclidean(Inf, 100))
 })
